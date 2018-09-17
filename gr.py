9968a6299ba010cb7691c0226fe9ba05453f25ae
jeevesh77 <42932891+jeevesh77@users.noreply.github.com>
2018-09-17 16:10:26 +0530

    Add files via upload

---
+++ gr.py
@@ -0,0 +1,24 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+if(a>b&a>c):
+	print("a is the greatest")
+elif(b>c):
+	print("b is greater")
+else:
+	print("c is the greatest")
+	#greatest of 3 numbers
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
---
+++ possitive.py
@@ -0,0 +1,5 @@
+num=int(input("enter the number"))
+if(num >0):
+	print("number is possitive")
+else:
+	print("number is negative")
