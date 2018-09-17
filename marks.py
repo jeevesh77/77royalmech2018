c92348eec5d8f6aac225fb8bd4aa84aad5978181
jeevesh77 <42932891+jeevesh77@users.noreply.github.com>
2018-09-17 16:21:41 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,22 @@
+marks=int(input("enter the number"))
+if(marks>=90):
+	print("grade A+")
+elif (marks>=80): 
+	print("grade A")
+elif (marks>=70):
+	print("grade B+")
+elif  (marks>=60):
+	print("grade B")
+elif  (marks>=50):
+	print("grade c")
+elif  (marks<40):
+	print("fail")
+#different grades
+
+
+
+
+
+
+
+
